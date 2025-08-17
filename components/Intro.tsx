d4f4a9904446a441e9a32251aa6be6ed5d418208
Prasanth Kumar <ch.prasanth2003@gmail.com>
2025-08-17 12:28:38 +0530

    Intro Update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -18,10 +18,10 @@ const Intro = () => {
         {/* I create stuff sometimes. */}Sometimes I like to build stuff.
       </h2>
       <p className="text-lg md:text-2xl text-[#8892b0] max-w-screen-sm p-4 mb-10">
-        I&apos;m a software engineer from Visakhapatnam, India, with a deep passion
+        I&apos;m a software engineer, with a deep passion
         for building large-scale, high-impact products in the fields of Machine
         Learning and Deep Learning. I&apos;m particularly driven by the opportunity
-        to leverage these technologies to solve real-world, critical problems.
+        to leverage these technologies to solve real-world problems.
       </p>
       <a
         className="border-2 px-6 py-4 rounded-lg flex flex-row gap-2 text-2xl cursor-pointer items-center transition-transform duration-300 hover:-translate-y-2"
