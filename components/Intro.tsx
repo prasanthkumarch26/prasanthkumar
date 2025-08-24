c05cced09a649731b2dc043c8dba47b2c4569a65
Prasanth Kumar <ch.prasanth2003@gmail.com>
2025-08-24 13:34:24 +0530

    Intro Update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -20,9 +20,7 @@ const Intro = () => {
       <p className="text-lg md:text-2xl text-[#8892b0] max-w-screen-sm p-4 mb-10">
         I&apos;m an aspiring software engineer with a deep passion for science
         and technology. I&apos;m driven to build innovative projects that use Machine
-        Learning and Deep Learning to solve real-world problems. I&apos;m eager to
-        bring my skills and curiosity to a team that is pushing the boundaries
-        of what&apos;s possible.
+        Learning and Deep Learning to solve real-world problems.
       </p>
       <a
         className="border-2 px-6 py-4 rounded-lg flex flex-row gap-2 text-2xl cursor-pointer items-center transition-transform duration-300 hover:-translate-y-2"
